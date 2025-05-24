105f9323b46bd54214d3f57aac9f2ba107cfef13
OmarMesqq <omarmsqt@gmail.com>
2025-05-24 12:49:06 -0300

    refactor: :memo: add comments

--- square.c
+++ square.c
@@ -1,6 +1,8 @@
 #include <CL/cl.h>
 #include <stdio.h>
 
+#define WORK_ITEMS 4
+
 int main() {
     FILE* f = fopen("square.cl", "r");
     if (!f) {
@@ -22,46 +24,70 @@ int main() {
     kernel_src[size] = '\0';
     fclose(f);
 
-    float data[4] = {1.0, 2.0, 3.0, 4.0}, out[4];
-    cl_platform_id pid; cl_device_id did; cl_context ctx;
-    cl_program prog; cl_kernel kern;
-    cl_mem din, dout;
+    // RAM allocated arrays
+    float data[WORK_ITEMS] = {1.0, 2.0, 3.0, 4.0};
+    float out[WORK_ITEMS];
 
-    clGetPlatformIDs(1, &pid, NULL);
-    clGetDeviceIDs(pid, CL_DEVICE_TYPE_GPU, 1, &did, NULL);
-    ctx = clCreateContext(NULL, 1, &did, NULL, NULL, NULL);
+    cl_platform_id platformId; cl_device_id deviceId; cl_context openClContext;
+    cl_program openClProgram; cl_kernel kern;
+
+    // Gets the first OpenCL platform (the GPU)
+    clGetPlatformIDs(1, &platformId, NULL);
+    // Gets the GPU from this platform
+    clGetDeviceIDs(platformId, CL_DEVICE_TYPE_GPU, 1, &deviceId, NULL);
+
+    openClContext = clCreateContext(NULL, 1, &deviceId, NULL, NULL, NULL);
+    
+    // Props of the command queue: where we can submit kernels to run
     cl_queue_properties props[] = {
-    CL_QUEUE_PROPERTIES, CL_QUEUE_PROFILING_ENABLE,
+        CL_QUEUE_PROPERTIES, 
+        CL_QUEUE_PROFILING_ENABLE,  // Track GPU time
         0
     };
 
-    cl_command_queue q = clCreateCommandQueueWithProperties(ctx, did, props, NULL);
+    cl_command_queue q = clCreateCommandQueueWithProperties(openClContext, deviceId, props, NULL);
 
+    // GPU RAM allocated arrays that receive in and out
+    cl_mem din, dout;
+    din = clCreateBuffer(openClContext, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(data), data, NULL);
+    dout = clCreateBuffer(openClContext, CL_MEM_WRITE_ONLY, sizeof(out), NULL, NULL);
 
-    din = clCreateBuffer(ctx, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, sizeof(data), data, NULL);
-    dout = clCreateBuffer(ctx, CL_MEM_WRITE_ONLY, sizeof(out), NULL, NULL);
+    // Create an OpenCL program object from kernel source code
+    openClProgram = clCreateProgramWithSource(openClContext, 1, &kernel_src, NULL, NULL);
 
-    prog = clCreateProgramWithSource(ctx, 1, &kernel_src, NULL, NULL);
-    clBuildProgram(prog, 1, &did, NULL, NULL, NULL);
-    kern = clCreateKernel(prog, "square", NULL);
+    // Compile the program for the target GPU using the OpenCL runtime and the device driver
+    clBuildProgram(openClProgram, 1, &deviceId, NULL, NULL, NULL);
 
+    // Gets the specific kernel from the compiled program (what actually runs on GPU)
+    kern = clCreateKernel(openClProgram, "square", NULL);
+
+    // Passes the _global float* in and out parameters
     clSetKernelArg(kern, 0, sizeof(cl_mem), &din);
     clSetKernelArg(kern, 1, sizeof(cl_mem), &dout);
 
-    size_t global = 4;
+    // How many GPU threads?
+    size_t workItems = WORK_ITEMS;
+    // OpenCL event for tracking execution time
     cl_event evt;
-    clEnqueueNDRangeKernel(q, kern, 1, NULL, &global, NULL, 0, NULL, &evt);
-    clWaitForEvents(1, &evt);
+
+    // Enqueue the "square" kernel in the command queue "q" to run across "workItems" threads and capture the event "evt"
+    clEnqueueNDRangeKernel(q, kern, 1, NULL, &workItems, NULL, 0, NULL, &evt);
+    clWaitForEvents(1, &evt);   // Wait for completion
+
+    // Copy contents of GPU RAM "dout" array to RAM "out" array
     clEnqueueReadBuffer(q, dout, CL_TRUE, 0, sizeof(out), out, 0, NULL, NULL);
 
+    // Profiling: get timestamps of kernel execution start and end
     cl_ulong start, end;
     clGetEventProfilingInfo(evt, CL_PROFILING_COMMAND_START, sizeof(cl_ulong), &start, NULL);
     clGetEventProfilingInfo(evt, CL_PROFILING_COMMAND_END, sizeof(cl_ulong), &end, NULL);
 
-    for (int i = 0; i < 4; i++) {
+    // Print results computed by the GPU and total GPU time
+    for (int i = 0; i < WORK_ITEMS; i++) {
         printf("%f^2 = %f\n", data[i], out[i]);
     }
-
     printf("GPU time (ns): %lu\n", end - start);
+
+    
     free(kernel_src);
 }
